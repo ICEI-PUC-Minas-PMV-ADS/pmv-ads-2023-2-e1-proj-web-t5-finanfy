b39cab8037a7d3c490296040ca70265ba0b9666a
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2023-11-21 14:45:34 -0300

    resolução comit

--- codigo-fonte/pages/Transacoes/transacoes.js
+++ codigo-fonte/pages/Transacoes/transacoes.js
@@ -1,15 +1,5 @@
-
-
-let transacoesJs = window.localStorage.getItem("db_transacoes");
-let transacoesObj = JSON.parse(transacoesJs);
-const lista = document.getElementById("lista");
-
-//fução para filtra transações do usuário logado
-for(const transacao of transacoesObj) {
-  if(transacao.idUsuario === usuarioLogado){
-    filtroTransacoes.push(transacao);
-  }
-}
+let transacoes = localStorage.getItem("db_transacoes");
+let transacoesObj = JSON.parse(transacoes);
 
 // ****************** FILTRO DE USUÁRIO LOGADO ****************** //
 
@@ -74,15 +64,57 @@ function ListaTransacoes() {
       li.appendChild(leftDiv);
       li.appendChild(rightDiv);
 
-        // Adiciona um ouvinte de eventos "click" a cada elemento "li"
-        li.addEventListener("click", () => {
-        // Recupera o ID da transação correspondente
-        const idDaTransacao = objeto.idTransacao;
-        // Redireciona o usuário para outra página para atualizar as informações
-        window.location.href = `../editar-transacoes/editar-transacoes.html?id=${idDaTransacao}`;
-      });
-
       lista.appendChild(li);
     });
   }
 }
+
+// ********************************************************** //
+
+const lista = document.getElementById("lista");
+
+filtroTransacoes.forEach((objeto) => {
+  const li = document.createElement("li");
+  li.classList.add("itemList");
+
+  const leftDiv = document.createElement("div");
+  leftDiv.classList.add("leftList");
+
+  const tituloP = document.createElement("p");
+  tituloP.classList.add("titleLabelList");
+  tituloP.textContent = objeto.descricao;
+
+  const dataP = document.createElement("p");
+  dataP.classList.add("dateLabelList");
+  dataP.textContent = objeto.data;
+
+  leftDiv.appendChild(tituloP);
+  leftDiv.appendChild(dataP);
+
+  const rightDiv = document.createElement("div");
+  rightDiv.classList.add("rightList");
+
+  const valorP = document.createElement("p");
+  valorP.classList.add("valueLabelList");
+  valorP.textContent = objeto.valor;
+
+  const horaP = document.createElement("p");
+  horaP.classList.add("hourLabelList");
+  horaP.textContent = objeto.hora;
+
+  rightDiv.appendChild(valorP);
+  rightDiv.appendChild(horaP);
+
+  li.appendChild(leftDiv);
+  li.appendChild(rightDiv);
+
+   // Adiciona um ouvinte de eventos "click" a cada elemento "li"
+   li.addEventListener("click", () => {
+    // Recupera o ID da transação correspondente
+    const idDaTransacao = objeto.idTransacao;
+     // Redireciona o usuário para outra página para atualizar as informações
+     window.location.href = `../editar-transacoes/editar-transacoes.html?id=${idDaTransacao}`;
+  });
+
+  lista.appendChild(li);
+});
